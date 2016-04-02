f4615831858122814bf0316eb4b01ef40d8010f0
Miguel Molina <hi@mvader.me>
2016-04-02 13:39:32 +0200

    Minor changes

--- LocalStorage.elm
+++ LocalStorage.elm
@@ -12,38 +12,38 @@ import Task exposing (Task)
 
 
 {-| Set a new element in localStorage with the given key and value.
+
     set "my-key" (Json.Encode.string "my-val")
 -}
-
 set : String -> Value -> Task () ()
 set key val =
   Native.LocalStorage.setItem key val
 
 
 {-| Retrieve an element from localStorage with the given key. It may be empty.
+
     case get "my-key" of
       Just val -> val
       Nothing -> "Nothing"
 -}
-
 get : String -> Maybe String
 get key =
   Native.LocalStorage.getItem key
 
 
 {-| Remove an element from localStorage with the given key.
+
     remove "my-key"
 -}
-
 remove : String -> Task () ()
 remove key =
   Native.LocalStorage.removeItem key
 
 
 {-| Clear all the elements in localStorage.
+
     clear ()
 -}
-
 clear : () -> Task () ()
 clear =
   Native.LocalStorage.clear
